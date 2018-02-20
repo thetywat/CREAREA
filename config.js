109bd7b72eefec3ef893523ffaa8d916a1f5ed5e
thetywat <thetywat@gmail.com>
2018-02-20 09:44:48 +0630

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '801',
+		wp_ws_url : 'http://localhost/wordpress/wp-appkit-api/801',
+		wp_url : 'http://localhost/wordpress',
+		theme : 'q-android',
+		version : '100',
+		app_title : 'WP',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'yTTZcYB.?f[<(,*NN8S#S^zNjJPv -ag<aPw^r+}5B0J{?/lR,$cZ%)DBJb&w0Mx',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
